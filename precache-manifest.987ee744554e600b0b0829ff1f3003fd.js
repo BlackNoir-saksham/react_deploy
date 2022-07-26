b2a33bc3b4d85e8b75790ae122c040aa4e34900c
Saksham Srivastava <sakshamabc22@gmail.com>
2022-07-26 12:14:06 +0530

    Updates

---
+++ precache-manifest.987ee744554e600b0b0829ff1f3003fd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2313a2b673757ee90a6ecd3a4905e8d",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "be1edaa52b44fdea62e1",
+    "url": "/react-deploy/static/css/main.a09ce7d7.chunk.css"
+  },
+  {
+    "revision": "73ba60b3b234bdbb75c6",
+    "url": "/react-deploy/static/js/2.c158cebb.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/react-deploy/static/js/2.c158cebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be1edaa52b44fdea62e1",
+    "url": "/react-deploy/static/js/main.0426f733.chunk.js"
+  },
+  {
+    "revision": "8000aa3098e9bc6599b2",
+    "url": "/react-deploy/static/js/runtime-main.58175d86.js"
+  },
+  {
+    "revision": "2bd8f5ae7253dd7bd9a4eded1d3e284a",
+    "url": "/react-deploy/static/media/ad.2bd8f5ae.png"
+  },
+  {
+    "revision": "a760194ced72846764c86c778eaf91c1",
+    "url": "/react-deploy/static/media/amazon-logo-transparent.a760194c.png"
+  },
+  {
+    "revision": "4d28b561fa87d68ea6c5c08796e9e718",
+    "url": "/react-deploy/static/media/banner_final.4d28b561.png"
+  },
+  {
+    "revision": "9a05cf8d8aa7c57ebbfa37c68fe90744",
+    "url": "/react-deploy/static/media/realme.9a05cf8d.jpg"
+  }
+]);
